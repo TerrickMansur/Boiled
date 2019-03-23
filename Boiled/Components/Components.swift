5de16b87234afce1367fa43ffb3e9bb0403a935c
Terrick Mansur <terrickmansur@gmail.com>
2019-03-23 11:31:26 +0100

    Removes styles from the components.

--- Boiled/Components/Components.swift
+++ Boiled/Components/Components.swift
@@ -18,36 +18,31 @@ public class Components {
 public protocol Componentable {
     
     associatedtype ViewModelType
-    associatedtype StyleType
 
     var viewModel: ViewModelType! { get set }
-    var style: StyleType { get set }
     
     static var storyboard: String { get }
 }
 
 public extension Componentable {
     
-    public static func create<GenericVC: Componentable>(viewModel: GenericVC.ViewModelType, style: GenericVC.StyleType) -> GenericVC where GenericVC : UIViewController {
+    public static func create<GenericVC: Componentable>(viewModel: GenericVC.ViewModelType) -> GenericVC where GenericVC : UIViewController {
         
         guard var component = (UIStoryboard.init(name: self.storyboard, bundle: Bundle.main).instantiateViewController(withIdentifier: self.storyboard) as? GenericVC) else {
             fatalError("Could not create component")
         }
         component.viewModel = viewModel
-        component.style = style
         
         return component
     }
     
-    public static func create<GenericVC: Componentable>(
-        style: GenericVC.StyleType,
-        viewModelProvider: (_ GenericVC: GenericVC) -> (GenericVC.ViewModelType)) -> GenericVC where GenericVC : UIViewController {
+    public static func create<GenericVC: Componentable>(viewModelProvider: (_ GenericVC: GenericVC) -> (GenericVC.ViewModelType)) -> GenericVC where GenericVC : UIViewController {
 
             guard var component = (UIStoryboard.init(name: self.storyboard, bundle: Bundle.main).instantiateViewController(withIdentifier: self.storyboard) as? GenericVC) else {
             fatalError("Could not create component") }
+
         component.loadViewIfNeeded()
         component.viewModel = viewModelProvider(component)
-        component.style = style
         
         return component
     }
